83fc553fd35757787dbf6d3ac62bf5556399e518
Abhishek-Santharaj <abhishek.santharaj@yahoo.com>
2021-08-06 22:21:13 +0530

    W Pattern

---
+++ WPattern.java
@@ -0,0 +1,34 @@
+public class WPattern {
+
+	public static void main(String[] args) {
+		int cap=7;int SecondSpace=0;
+		for(int row=0;row<=3;row++){
+	
+		for(int space=1;space<=row;space++)
+		{
+			System.out.print("  ");
+		}
+		for(int col=1;col<=cap;col++)
+		{
+			if(col==1 || col==cap)
+				System.out.print(" *");
+			else
+				System.out.print("  ");
+		}
+		if(row>=1)
+			{	
+				for(int space=1;space<=SecondSpace;space++)
+				{System.out.print("  ");}
+			}
+		for(int col=1;col<=cap;col++)
+		{
+			if(col==1 || col==cap)
+				System.out.print(" *");
+			else
+				System.out.print("  ");
+		}
+		
+		System.out.println();cap-=2;SecondSpace+=2;
+		}
+		}
+}
