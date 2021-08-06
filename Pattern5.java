c10d9e627be8b00fb11f3a89bc9d0506bb412a62
Abhishek-Santharaj <abhishek.santharaj@yahoo.com>
2021-08-06 19:35:35 +0530

    5

--- Pattern5.java
+++ Pattern5.java
@@ -1,15 +1,15 @@
 public class Pattern5 {
 
 	public static void main(String[] args) {
-		int no=1,cap=3;
-		for(int row=1;row<=3;row++)
+		int no=1;
+		for(int row=3;row>=1;row--)
 		{
-		for(int col=1;col<=cap;col++)
+		for(int col=1;col<=row;col++)
 		{
 		System.out.print(no+" ");
 		no++;
 		}
-		System.out.println();cap--;
+		System.out.println();
 		}
 		
 	}
