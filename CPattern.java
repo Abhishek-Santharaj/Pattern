66d10de62ab28ed26f197aa7281392d8b1c8816d
Abhishek-Santharaj <abhishek.santharaj@yahoo.com>
2021-08-06 22:36:46 +0530

    C Pattern

---
+++ CPattern.java
@@ -0,0 +1,31 @@
+public class CPattern {
+
+	public static void main(String[] args) 
+{
+		for(int col=1;col<=6;col++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println();
+		for(int col=1;col<=4;col++)
+		{
+		System.out.println("* ");
+		}
+		for(int col=1;col<=6;col++)
+		{
+		System.out.print("* ");
+		}
+}
+}
+	
+/* Output 
+
+* * * * * * 
+* 
+* 
+* 
+* 
+* * * * * * 
+
+
+*/
