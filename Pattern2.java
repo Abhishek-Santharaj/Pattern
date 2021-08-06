a5bdfd082a15f4fa5fd20eba3a36ebc57f75ec15
Abhishek-Santharaj <abhishek.santharaj@yahoo.com>
2021-08-06 18:54:12 +0530

    Pattern Programs

---
+++ Pattern2.java
@@ -0,0 +1,31 @@
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=5;row++) 
+		{
+		for(int col=row;col<=5;col++)
+		{		
+		System.out.print(" ");
+		}
+		for(int col=1;col<=row;col++)
+		{
+		System.out.print(col + " ");
+		}
+		System.out.println();
+		}
+		
+
+	}
+
+}
+
+
+/* Output
+     1 
+    1 2 
+   1 2 3 
+  1 2 3 4 
+ 1 2 3 4 5 
+ 
+ */
